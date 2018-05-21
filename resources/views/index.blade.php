bd1fe9890dd982d8d97f6b88a2d65b10e415972d
vagrant <vagrant@homestead>
2018-05-21 23:48:23 +0000

    Added cover, and different sections for profile and experience.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,8 +2,38 @@
 <html lang="{{ app()->getLocale() }}">
     <head>
         <title>Cameron Adams - Software Engineer</title>
+
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
     <body>
+    <section id="top">
+        <div class="wrapper-bg-img">
+            <div class="wrapper-nav">
+                <nav class="main-nav">
+                    <div class="nav-column-1">
+                        <a href="#about">About</a>
+                    </div>
+                    <div class="nav-column-2">
+                        <a href="#">Work</a>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </section>
 
+    <section id="profile">
+        <div class="wrapper-main">
+            <h1 class="header-profile">Profile</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid atque, cumque facilis harum inventore minus neque nihil obcaecati qui. Accusantium alias deleniti dicta, doloremque dolorum ducimus error est et exercitationem id in magnam nemo officia omnis, recusandae! Accusamus accusantium, blanditiis cumque delectus deserunt et fuga fugiat hic illo impedit in ipsa libero minima nobis nulla quod quos, reprehenderit voluptatem? Alias commodi, dolore eius eligendi eveniet exercitationem hic ipsa, iusto labore maiores molestiae, nemo non officiis pariatur quasi quisquam quod repellat velit! Blanditiis, dicta eius excepturi explicabo fugiat fugit labore molestiae perferendis quaerat quam sequi soluta veniam voluptate? Dolorem exercitationem iste labore, neque possimus sapiente unde? Ad cum dolorum esse exercitationem facere fuga, ipsa magni odit sequi voluptas? Dolores impedit itaque iure odit optio. Debitis deserunt eius eveniet ipsam iste necessitatibus quod tempora. Ab alias amet consequatur doloribus necessitatibus omnis quasi, velit? Dicta dolores est harum iusto quisquam reiciendis saepe temporibus.</p>
+        </div>
+    </section>
+
+    <section id="experience">
+        <div class="wrapper-main">
+            <h1 class="header-profile">Experience</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur at consequatur dolorum, exercitationem expedita in iusto laborum maiores, nam necessitatibus officia perferendis quaerat quo reprehenderit sed sit, sunt totam! Ab aliquid autem culpa distinctio eos error explicabo ipsam laborum maiores minima modi molestias, nemo, nihil nobis, non odit porro provident.</p>
+        </div>
+    </section>
     </body>
 </html>
